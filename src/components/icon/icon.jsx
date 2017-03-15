c55b2334fb0afca2b814424df058ffa55075cdef
lzkui2013 <1073869701@qq.com>
2017-03-16 07:37:32 +0800

    添加icon组件

---
+++ src/components/icon/icon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Icon extends React.Component {
+    addClick () {
+        alert('')
+    }
+    render() {
+        const baseClassName = `ireact-icon ireact-icon-${this.props.type}`;
+        return <span onClick={this.addClick} className={baseClassName}></span>
+    }
+}
--- src/view/index/index.jsx
+++ src/view/index/index.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import {Link} from 'react-router';
-import Headers from '../../components/header/header.jsx';
+import IndexHeader from './indexHeader.jsx';
 import './index.less';
 
 export default class Index extends React.Component {
 	render () {
-		return <Headers>
-					<img className="logo" src={require('../../assets/img/logo.jpg')} />
-		            <Link to="/download" className="download">
-		                <span className="head_text">下载客户端</span>
-		            </Link>
-				</Headers>
+		return <IndexHeader />
 	}
 }
